e20e62cdb57a8cdf9138bafaab2829386f905072
Ivanilson Freitas <ivanilson.freitas@fmsa.hec.local>
2021-12-27 23:20:12 -0300

    Iniciando projeto!

---
+++ src/main/java/one/digitalinnovation/ivanilson/personapi/PersonapiApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.ivanilson.personapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonapiApplication.class, args);
+	}
+
+}
