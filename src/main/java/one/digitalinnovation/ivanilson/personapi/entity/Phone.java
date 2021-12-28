75ebdec7f049c10da20791af722884ffce698c62
ivanffreitas <ivanilson.freitas@fmsa.hec.local>
2021-12-28 07:37:50 -0300

    Modelando o dominio da aplicação

---
+++ src/main/java/one/digitalinnovation/ivanilson/personapi/entity/Phone.java
@@ -0,0 +1,29 @@
+package one.digitalinnovation.ivanilson.personapi.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import one.digitalinnovation.ivanilson.personapi.enums.PhoneType;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Phone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //Auto incremento
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PhoneType type;
+
+    @Column(nullable = false)
+    private String number;
+
+}
